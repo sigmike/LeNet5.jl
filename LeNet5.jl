5dfa2c9b25abcfd7458926a6efb2af39c4b81ace
Michael Witrant <michael@ouvrages-web.fr>
2013-09-08 14:55:03 +0200

    6 feature maps

--- LeNet5.jl
+++ LeNet5.jl
@@ -4,10 +4,12 @@ using Base.Test
 
 function c1(input, weights, bias)
     @assert size(input) == (32,32)
-    output = zeros(28,28)
-    for i in 1:28
-        for j in 1:28
-            output[i, j] = squash(sum(input[i:i+5-1, j:j+5-1] .* weights) + bias)
+    output = zeros(6,28,28)
+    for feature_map in 1:6
+        for i in 1:28
+            for j in 1:28
+                output[feature_map, i, j] = squash(sum(input[i:i+5-1, j:j+5-1] .* weights[feature_map]) + bias[feature_map])
+            end
         end
     end
     output
@@ -23,14 +25,15 @@ end
 @test_approx_eq_eps -1.0 squash(-1) 1e-5
 
 function test_c1()
+    srand(123)
     input = rand(32,32)
-    weights = rand(5,5)
-    bias = rand()
+    weights = rand(6, 5,5)
+    bias = rand(6)
 
     output = c1(input, weights, bias)
-    @test size(output) == (28,28)
-    @test output[1,1] == squash(sum(input[1:5, 1:5] .* weights) + bias) 
-    @test output[5,2] == squash(sum(input[5:9, 2:6] .* weights) + bias) 
+    @test size(output) == (6,28,28)
+    @test output[1, 1, 1] == squash(sum(input[1:5, 1:5] .* weights[1]) + bias[1]) 
+    @test output[3, 5, 2] == squash(sum(input[5:9, 2:6] .* weights[3]) + bias[3]) 
 end
 
 end
