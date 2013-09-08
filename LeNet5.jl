33e591ebdbf9882ed588251d7ac2472142dfd07b
Michael Witrant <michael@ouvrages-web.fr>
2013-09-08 17:21:38 +0200

    stored weights

--- LeNet5.jl
+++ LeNet5.jl
@@ -2,13 +2,18 @@ module LeNet5
 
 using Base.Test
 
-function c1(input, weights, bias)
+type C1
+    weights
+    biases
+end
+
+function run(layer::C1, input)
     @assert size(input) == (32,32)
     output = zeros(6,28,28)
     for feature_map in 1:6
         for i in 1:28
             for j in 1:28
-                output[feature_map, i, j] = squash(sum(input[i:i+5-1, j:j+5-1] .* weights[feature_map]) + bias[feature_map])
+                output[feature_map, i, j] = squash(sum(input[i:i+5-1, j:j+5-1] .* layer.weights[feature_map]) + layer.biases[feature_map])
             end
         end
     end
@@ -27,13 +32,12 @@ end
 function test_c1()
     srand(123)
     input = rand(32,32)
-    weights = rand(6, 5,5)
-    bias = rand(6)
+    c1 = C1(rand(6, 5,5), rand(6))
 
-    output = c1(input, weights, bias)
+    output = run(c1, input)
     @test size(output) == (6,28,28)
-    @test output[1, 1, 1] == squash(sum(input[1:5, 1:5] .* weights[1]) + bias[1]) 
-    @test output[3, 5, 2] == squash(sum(input[5:9, 2:6] .* weights[3]) + bias[3]) 
+    @test output[1, 1, 1] == squash(sum(input[1:5, 1:5] .* c1.weights[1]) + c1.biases[1])
+    @test output[3, 5, 2] == squash(sum(input[5:9, 2:6] .* c1.weights[3]) + c1.biases[3])
 end
 
 end
